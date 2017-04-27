6d4ed6848d23f11d1e5e0fb5d0c0edc4add71d81
Paul Alcaraz <paulalcaraz@gmail.com>
2017-04-27 16:44:38 -0700

    redo link back func

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path 
     else
       redirect_to new_user_path
     end
